3fbb5eaa238fd4de20db44275c55f3915a6513b4
jarbirS <jarbirstewartmr@ufps.edu.co>
2020-05-07 07:57:17 -0500

    Colocando los js

--- js/js.js
+++ js/js.js
@@ -1,1 +1,8 @@
-functiom
+
+	$("#registro").click(function(event){
+		$("#contenedor").load('registro/registro.html');
+	});
+
+	$("#iniciar").click(function(event){
+		$("#contenedor").load('login/login.html');
+	});
