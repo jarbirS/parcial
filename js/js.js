1a690b82087c878e0ea474725d33664021204739
jarbirS <jarbirstewartmr@ufps.edu.co>
2020-05-07 09:50:49 -0500

    mejorando el previo

--- js/js.js
+++
@@ -1,8 +0,0 @@
-
-	$("#registro").click(function(event){
-		$("#contenedor").load('registro/registro.html');
-	});
-
-	$("#iniciar").click(function(event){
-		$("#contenedor").load('login/login.html');
-	});
